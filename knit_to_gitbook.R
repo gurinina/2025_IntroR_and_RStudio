8a697894194975c6202e32fd1b79750c48b3a30f
gurinina <g.giaever@ubc.ca>
2024-09-12 21:55:03 -0700

    16-tidyverse

--- knit_to_gitbook.R
+++ knit_to_gitbook.R
@@ -3,3 +3,45 @@ bookdown::render_book("index.Rmd", "bookdown::gitbook")
 
 # Open the resulting GitBook in the Viewer pane
 rstudioapi::viewer("_book/index.html")
+
+check_code_fences <- function(file) {
+  # Read the file
+  content <- readLines(file)
+
+  # Initialize a counter for code fences
+  fence_count <- 0
+
+  # Loop through each line to check for fences
+  for (i in seq_along(content)) {
+    line <- content[i]
+
+    # Check if the line contains a code fence (``` or ```{r})
+    if (grepl("^```", line)) {
+      fence_count <- fence_count + 1
+    }
+  }
+
+  # Final check: Output only if the number of fences is odd (i.e., unbalanced)
+  if (fence_count %% 2 != 0) {
+    cat("Code fences are unbalanced in file:", file, "\n")
+  } else {
+    cat("Code fences are balanced in file:", file, "\n")
+  }
+}
+
+# Example usage: check your Rmd or md file
+check_code_fences("path/to/your_file.md")
+
+
+collapsible_code <- function(html_content, non_html_content) {
+  output_format <- knitr::opts_knit$get("rmarkdown.pandoc.to")
+  # opts_knit$get("out.format") is not reliable for determining the output format
+  # print(c("format", output_format))
+  if (output_format == "html") {
+    # HTML output (GitBook/HTML): Show collapsible section
+    cat(html_content)
+  } else {
+    # Non-HTML output (EPUB, PDF): Show regular R code block
+    cat(non_html_content)
+  }
+}
--- render_all.R
+++ render_all.R
@@ -1,6 +1,14 @@
+library(bookdown)
+
+clean_book(clean = TRUE)
+
 
 # Render GitBook format
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
+bookdown::render_book("index.Rmd", "bookdown::gitbook", quiet = FALSE)
+
+# After Gitbook is rendered, open 16-etidyverse.md in rsconnect and save it as
+# 16-tidyverse.Rmd in the lessons directory
+# Then run EPUB and PDF renderings
 
 # Render EPUB format
 bookdown::render_book("index.Rmd", "bookdown::epub_book")
@@ -8,3 +16,6 @@ bookdown::render_book("index.Rmd", "bookdown::epub_book")
 # Render PDF format
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 
+
+# After EPUB and PDF have rendered, open 16-wtidyverse.md in rsconnect and save it as
+# 16-tidyverse.Rmd in the lessons directory
